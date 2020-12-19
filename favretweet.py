9ef8ad6b88b0dde26d8ce6461b0273dcf076438a
gustavoghp87 <ghp.2120@gmail.com>
2020-12-19 17:42:53 -0300

    url, dockerfile

--- favretweet.py
+++ favretweet.py
@@ -18,7 +18,7 @@ class FavRetweetListener(tweepy.StreamListener):
         self.me = api.me()
         starttime = time.time()
 
-        connect = "mongodb+srv://maslabook-bot7200:" + config('ATLAS_DB') + "@cluster0.mykp7.mongodb.net/bot7200?retryWrites=true&w=majority"
+        connect = config('ATLAS_DB')
         myclient = pymongo.MongoClient(connect)
         mydb = myclient["bots"]
         mycol = mydb["bot7200"]
